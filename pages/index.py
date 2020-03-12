66244ba5e58e5f069eaad27789076067950726b9
Robert Buckley <robertworkbuckley@gmail.com>
2020-03-12 15:42:07 -0700

    updates

--- pages/index.py
+++ pages/index.py
@@ -16,17 +16,13 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
+            ## Find the Driving Accuracy:
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            Use a few known stats to predict the likley driving accuracy of a PGA player.
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Predict Here!', color='primary'), href='/predictions')
     ],
     md=4,
 )
